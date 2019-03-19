afe062592d842350bafb2422212f278ed909c56d
wangyh <18819467659@163.com>
2019-03-19 22:42:43 +0800

    add route

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const http = require('http')
 const chalk = require('chalk')
+const path = require('path')
 const conf = require('./config/defaultConfig')
+const route = require('./helper/route')
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('demo')
+  const filePath = path.join(conf.root, req.url)
+  route(req, res, filePath)
 })
 
 server.listen(conf.port, conf.hostname, () => {
---
+++ src/helper/route.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const path = require('path')
+const Handlebars = require('handlebars')
+const chalk = require('chalk')
+const promisify = require('util').promisify
+const stat = promisify(fs.stat)
+const readdir = promisify(fs.readdir)
+const config = require('../config/defaultConfig')
+const mime = require('../helper/mime')
+
+const tplPath = path.join(__dirname, '../template/dir.tpl')
+const source = fs.readFileSync(tplPath)
+const template = Handlebars.compile(source.toString())
+
+module.exports = async function(req, res, filePath) {
+  try {
+    const stats = await stat(filePath)
+    if (stats.isFile()) {
+      const contentType = mime(filePath)
+      res.statusCode = 200;
+      res.setHeader("Content-Type", contentType);
+      fs.createReadStream(filePath).pipe(res);
+    } else if (stats.isDirectory()) {
+      const files = await readdir(filePath)
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      const dir = path.relative(config.root, filePath)
+      const data = {
+        title: path.basename(filePath),
+        dir: dir ? `/${dir}` : '',
+        files: files.map(file => {
+          return {
+            file,
+            icon: mime(file)
+          }
+        })
+      }
+      res.end(template(data));
+    }
+  } catch (e) {
+    console.error(chalk.red(e))
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(`${filePath} is not a directory or file\n ${e}`);
+  }
+}
