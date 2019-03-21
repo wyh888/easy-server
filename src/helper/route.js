9a2611e32e2bb80a517d08edf839018f71aa487c
wangyh <18819467659@163.com>
2019-03-21 22:04:07 +0800

    add compress

--- src/helper/route.js
+++ src/helper/route.js
@@ -7,6 +7,8 @@ const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
 const config = require('../config/defaultConfig')
 const mime = require('../helper/mime')
+const compress = require('../helper/compress')
+const range = require('../helper/range')
 
 const tplPath = path.join(__dirname, '../template/dir.tpl')
 const source = fs.readFileSync(tplPath)
@@ -17,13 +19,24 @@ module.exports = async function(req, res, filePath) {
     const stats = await stat(filePath)
     if (stats.isFile()) {
       const contentType = mime(filePath)
-      res.statusCode = 200;
-      res.setHeader("Content-Type", contentType);
-      fs.createReadStream(filePath).pipe(res);
+      res.setHeader("Content-Type", contentType)
+      let rs
+      const {code, start, end} = range(stats.size, req, res)
+      if (code === 200) {
+        res.statusCode = 200
+        rs = fs.createReadStream(filePath)
+      } else {
+        res.statusCode = 206
+        rs = fs.createReadStream(filePath, {start, end})
+      }
+      if (filePath.match(config.compress)) {
+        rs = compress(rs, req, res)
+      }
+      rs.pipe(res)
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath)
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
+      res.statusCode = 200
+      res.setHeader("Content-Type", "text/html")
       const dir = path.relative(config.root, filePath)
       const data = {
         title: path.basename(filePath),
